a2293ac30d67ceb12e7dbe540d03cf0f77433ac3
Aditya Kumar <way2adityakumar@gmail.com>
2025-10-26 15:47:23 +0530

    fixed build

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import React, { Suspense, lazy } from "react";
+import { Suspense, lazy } from "react";
+import "./index.css";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -8,7 +9,6 @@ import Error from "./components/Error";
 import Store from "./components/Store";
 import { Outlet } from "react-router-dom";
 import CaseCardsOptions from "./components/CaseCardsOptions";
-import Shimmer from "./components/Shimmer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -36,13 +36,7 @@ const appRouter = createBrowserRouter([
       {
         path: "/categories",
         element: (
-          <Suspense
-            fallback={
-              <h1>
-                <Shimmer />
-              </h1>
-            }
-          >
+          <Suspense fallback={<h1>Wait</h1>}>
             <Categories />
           </Suspense>
         ),
