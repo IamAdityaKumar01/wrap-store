a77510a3e00df7eeacd11038632a6f8ca7340ac4
Aditya Kumar <way2adityakumar@gmail.com>
2025-11-11 19:41:40 +0530

    fireEvent testing on button

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -2,11 +2,10 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
-import { screen, render } from "@testing-library/react";
-
+import { screen, render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-it("should load Header component", () => {
+it("Should load Header component", () => {
   render(
     <Provider store={appStore}>
       <BrowserRouter>
@@ -16,4 +15,32 @@ it("should load Header component", () => {
   );
 
   const input = screen.getByRole("textbox");
+  expect(input).toBeInTheDocument();
+});
+
+it("Should load 0 (items in cart) on Header component", () => {
+  render(
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </Provider>
+  );
+
+  const cartValue = screen.getByText("0");
+  expect(cartValue).toBeInTheDocument();
+});
+
+it("Should click login button", () => {
+  render(
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </Provider>
+  );
+
+  const LoginButton = screen.getByRole("button", { name: "Login" });
+  fireEvent.click(LoginButton);
+  expect(LoginButton).toBeInTheDocument();
 });
