1e51ee91d7f6d3c1141180f8956d4ce747351288
Aditya Kumar <way2adityakumar@gmail.com>
2025-11-11 16:16:38 +0530

    Added more test Cases

--- src/components/__tests__/Login.test.js
+++ src/components/__tests__/Login.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import Login from "../Login";
 import "@testing-library/jest-dom";
-test("should load Login component", () => {
-  render(<Login />);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
+
+describe("Testing for Login component", () => {
+  test("should load Login component", () => {
+    render(<Login />);
+
+    const heading = screen.getByRole("heading");
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("should load all buttons in Login component", () => {
+    render(<Login />);
+
+    const button = screen.getAllByRole("button");
+
+    expect(button.length).toBe(4);
+  });
 });
